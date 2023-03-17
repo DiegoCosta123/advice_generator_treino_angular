b50489918904c9f462e0ab97da98c2f21ddb4839
Diego da Costa <diegoccpinto@gmail.com>
2023-03-17 09:47:55 -0300

    Ajustando as frases aleatorio foi feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'advice-generator-treino';
+  textToShow: string = "It is easy to sit up and take notice, what's difficult is getting up and taking action.";
+
+  people: string[] = [ "Tornou-se chocantemente óbvio que a nossa tecnologia excedeu a nossa humanidade.",
+                    "Trocava toda a minha tecnologia por uma tarde com Sócrates.",
+                    "Muitas palavras não indicam necessariamente muita sabedoria." ];
+
+  buttonClick() {
+    this.Frase();
+  }
+
+  Frase() {
+    this.textToShow = this.people[Math.floor(Math.random() * this.people.length)];
+  }
 }
+
